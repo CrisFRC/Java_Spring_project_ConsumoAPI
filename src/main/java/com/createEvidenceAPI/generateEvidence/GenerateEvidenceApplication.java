66a068fecc03bdc7a20e5ee9ade5c92ad170938f
CrisFRC <ing.cristian.roac@gmail.com>
2023-01-23 21:59:48 -0500

    inicio proyecto vacio spring

---
+++ src/main/java/com/createEvidenceAPI/generateEvidence/GenerateEvidenceApplication.java
@@ -0,0 +1,13 @@
+package com.createEvidenceAPI.generateEvidence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateEvidenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateEvidenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/createEvidenceAPI/generateEvidence/GenerateEvidenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.createEvidenceAPI.generateEvidence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateEvidenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
